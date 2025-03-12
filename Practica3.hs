10a83fb8f0fc67217a36496533a87021473f6148
Erick <ericklj0509_@ciencias.unam.mx>
2025-03-11 22:29:47 -0600

    Cambios

--- Practica3.hs
+++ Practica3.hs
@@ -33,9 +33,33 @@ instance Show Prop where
 
 -- 3.2 Formas Normales
 
--- Ejercicio 1
+--Funcion auxiliar que devuelve la negacion de la formula ingresada
+negar :: Prop -> Prop
+negar (Var p) = Not (Var p)
+negar (Cons True) = (Cons False)
+negar (Cons False) = (Cons True)
+negar (Not f) = f
+negar (And f1 f2) = (Or (negar f1) (negar f2))
+negar (Or f1 f2) = (And (negar f1) (negar f2))
+negar (Impl f1 f2) = (And f1 (negar f2))
+negar (Syss f1 f2)= negar (And (Impl f1 f2) (Impl f2 f1))
+
+--Funcion auxiliar que aplica la distribucion de la disyuncion parcialmente
+distribuir :: Prop -> Prop
+distribuir (Or p (And q r)) = And (distribuir (Or p q)) (distribuir (Or p r))
+distribuir (Or (And q r) p) = And (distribuir (Or q p)) (distribuir (Or r p))
+distribuir (Or p q) = Or (distribuir p) (distribuir q)
+distribuir (And p q) = And (distribuir p) (distribuir q)
+distribuir p = p
+
+-- Funcion que convierte una formula proposicional en su forma normal negativa
 fnn :: Prop -> Prop
-fnn = undefined
+fnn (Not p) = negar (fnn p)
+fnn (And p q) = And (fnn p) (fnn q)
+fnn (Or p q) = Or (fnn p) (fnn q)
+fnn (Impl p q) = fnn (Or (negar p) q)
+fnn (Syss p q) = fnn (And (Impl p q) (Impl q p))
+fnn p = p
 
 -- Ejercicio 2
 fnc :: Prop -> Prop
@@ -53,11 +77,19 @@ type Clausula = [Literal]
 
 -- Ejercicios
 
--- Ejercicio 1
-clausulas :: Prop -> [Clausula]
-clausulas = undefined
+-- Función auxiliar para procesar disyunciones
+processOR :: Prop -> Clausula
+processOR (Or p q) = processOR p ++ processOR q
+processOR p = [p]  
 
--- Ejercicio 2
+-- Función para extraer cláusulas de una fórmula en FNC
+clausulas :: Prop -> [Clausula]
+clausulas (And p q) = clausulas p ++ clausulas q 
+clausulas (Or p q) = [processOR (Or p q)] 
+clausulas p = [[p]]  
+
+
+-- Ejercicio 2clau
 resolucion :: Clausula -> Clausula -> Clausula
 resolucion = undefined
 
@@ -66,9 +98,16 @@ resolucion = undefined
 
 -- Ejercicios
 
--- Ejercicio 1
+-- Función auxiliar para verificar si dos literales son complementarios
+esComplementario :: Literal -> Literal -> Bool
+esComplementario (Not l1) l2 = l1 == l2
+esComplementario l1 (Not l2) = l1 == l2
+esComplementario _ _ = False
+
+-- Función que determina si es posible obtener un resolvente a partir de 2 clausulas
 hayResolvente :: Clausula -> Clausula -> Bool
-hayResolvente = undefined
+hayResolvente clausula1 clausula2 =
+    any (\l1 -> any (\l2 -> esComplementario l1 l2) clausula2) clausula1
 
 -- Ejercicio 2
 saturacion :: Prop -> Bool
